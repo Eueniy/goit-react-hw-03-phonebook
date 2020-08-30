7962b378852fe19c71e150f7b6191befc780c6c9
Eueniy <tkachev.eugeny@gmail.com>
2020-08-30 23:55:50 +0300

    Updates

---
+++ precache-manifest.a917aac9d3f0fd26bc43e5c32565a39e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e193917c85fdb3206b75fcb3cd37a4",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "71d786621a936a9b4917",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e760438a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e760438a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f594c8e3161fb9773eaf",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.3449f215.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
